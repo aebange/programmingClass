cc8fb84867ba23347cfeca0d0d67dc18a723cc54
aebange <aebange@gmail.com>
2018-07-22 16:17:32 -0400

    Added a clear screen feature to each break.
    Signed-off-by: aebange <aebange@gmail.com>

--- Python/lessons/randomNumberGenerator.py
+++ Python/lessons/randomNumberGenerator.py
@@ -1,31 +1,34 @@
 import random
 import sys
+import os
 minimumNumber = 1
 maximumNumber = 100
 totalGuessed = 0
 # Create the random number
 randomNumber = random.randint(minimumNumber,maximumNumber)
-print("Welcome to the guessing game")
+print("Welcome to the guessing game!")
 
 
 # Request user input and ensure that it is both a number within the range and a number that is an integer
 def get_clean_number(min, max):
     while True:
-        print("Enter a number between %d %d" % (min, max))
+        print("Enter a number between %d and %d." % (min, max))
         raw_guess = input()
         if raw_guess.isnumeric():
             # User entered a numeric value
             local_clean_guess = int(raw_guess)
             if local_clean_guess < min:
                 # User entered a number that was outside the range
-                print("Your number is too low")
+                os.system("cls")
+                print("Your number is too low.")
             elif local_clean_guess > max:
-                print("Your number is too high")
+                print("Your number is too high.")
             else:
                 # Passes the value of localCleanGuess to whatever variable the function is assigned to
                 return local_clean_guess
         else:
-            # User entered a string or something strange
+            # User entered a string or something strange - clearing screen
+            os.system("cls")
             print("Huh?")
 
 
@@ -37,10 +40,16 @@ while True:
 
     # Checks to see if the guess is too low or too high
     if cleanGuess > randomNumber:
-        print("You entered a number that was too high")
+        os.system('cls')
+        print("Your guess of %d was too high, this attempt number %d." % (cleanGuess, totalGuessed))
     elif cleanGuess < randomNumber:
-        print("You entered a number that was too low")
+        os.system('cls')
+        print("Your guess of %d was too low, this attempt number %d." % (cleanGuess, totalGuessed))
     else:
-        print("Congratulations, you guessed the number!")
+        os.system('cls')
+        print("Congratulations, you successfully guessed the number!")
         print("Took you %d guesses.  The random number was %d." % (totalGuessed, randomNumber))
+        print("Press any key to continue...")
+        # Use the input as a break point to prevent the program from closing before the user can read the results.
+        i = input()
         sys.exit(0)
